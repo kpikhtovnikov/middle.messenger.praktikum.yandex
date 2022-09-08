c16deeacb80314e09dc522c4564644b77e0e1761
kpikhtovnikov <pihtovnikovkirill@gmail.com>
2022-09-09 00:29:35 +0300

    delete fallback

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-const fallback = require('express-history-api-fallback');
 const express = require('express');
 
 const app = express();
@@ -6,7 +5,6 @@ const app = express();
 const { PORT = 3000 } = process.env;
 
 app.use(express.static(__dirname + '/dist/'));
-app.use(fallback('index.html', { root: __dirname + '/dist/'}));
 
 app.get('/', function(req, res) {
   res.sendFile(path.resolve(__dirname, "/dist"));
