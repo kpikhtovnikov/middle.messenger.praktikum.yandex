3385ed7e216288f0b48fcf73f82cacc405c20c95
kpikhtovnikov <pihtovnikovkirill@gmail.com>
2022-09-08 23:46:39 +0300

    some changes for correct redirects

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 const { PORT = 3000 } = process.env;
 
 app.use(express.static(__dirname + '/dist/'));
-app.use(fallback('../dist/index.html', { root: __dirname }));
+app.use(fallback('index.html', { root: __dirname + '/dist/'}));
 
 app.get('/', function(req, res) {
   res.sendFile(path.resolve(__dirname, "/dist"));
