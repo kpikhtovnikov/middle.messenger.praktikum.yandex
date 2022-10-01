a167a7d0cf70f72964ce2665534b0734f5146ff4
kpikhtovnikov <pihtovnikovkirill@gmail.com>
2022-10-01 19:30:43 +0300

    add consts and types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,26 @@
-export { InputType, ButtonType, ChatType, MessageProps } from './types';
+export {
+    SignupType,
+    SigninType,
+    BlockClass,
+    InputType,
+    ButtonEnum,
+    Screens,
+    ChatType,
+    MessageProps,
+    props,
+    CreateChatType,
+    RemoveChatType,
+    SearchUserByLoginType,
+    AddUserToChatType,
+    GetChatTokenType,
+    GetUserForChatType,
+    UserInfoDTO,
+    UserInfoType,
+    UserPasswordType,
+    StoreEvents,
+    MessageDTO,
+    RemoveUserFromChat,
+    InitialStateType,
+    UserType,
+  } from './types';
+  
